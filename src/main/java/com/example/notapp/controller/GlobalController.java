04c77f2da0b91580c2f637b67b7b7460af74a21d
Emircan BEYAN <emircan.beyan@std.medipol.edu.tr>
2025-06-11 15:32:42 +0300

    add actuator dependency and implement global exception handling controller

---
+++ src/main/java/com/example/notapp/controller/GlobalController.java
@@ -0,0 +1,30 @@
+package com.example.notapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;  // ResponseEntity için
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.notapp.model.Grade;
+import com.example.notapp.model.Student;
+import com.example.notapp.repository.GradeRepository;
+import com.example.notapp.repository.StudentRepository;
+
+@RestControllerAdvice
+public class GlobalController {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+}
